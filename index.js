79a7b4c6acb498d1553a5c46b7252196d023981b
Youssef Abdelhamid <ya1027@nyu.edu>
2022-03-27 15:30:06 +0200

    add headers

--- index.js
+++ index.js
@@ -13,6 +13,13 @@ app.get("/", (req, res) => {
   res.send("Zaagel");
 });
 
+// headers
+app.use(function (req, res, next) {
+  res.setHeader('Access-Control-Allow-Origin', 'http://zaagel.samuraisoftware.house');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  next();
+});
+
 const mailRoute = require("./routes/mail");
 app.use("/mail", mailRoute);
 
