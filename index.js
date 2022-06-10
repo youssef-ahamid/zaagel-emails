7b79ceffd4324605c29f2c3b1b813f6ac68559b2
youssef-ahamid <58043372+youssef-ahamid@users.noreply.github.com>
2022-06-10 15:21:42 +0200

    allow any cors origin

--- index.js
+++ index.js
@@ -2,7 +2,9 @@ const express = require("express");
 const app = express();
 
 const cors = require("cors");
-app.use(cors())
+app.use(cors({
+  origin: "*"
+}))
 
 const dotenv = require("dotenv");
 dotenv.config();
